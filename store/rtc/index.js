075454d51da11edc834e23f4e574db65ec0c5e98
NTS1004 <1309022685@qq.com>
2022-02-05 00:25:06 +0800

    完成语音通话和视频聊天的操作按钮布局

--- store/rtc/index.js
+++ store/rtc/index.js
@@ -6,21 +6,16 @@ const default_state = {
 	// 是否群聊
 	grounp: false,
 	// 连接的用户信息
-    rtc_info: undefined
+    rtc_info: undefined,
+	// 麦克风是否打开
+	microPhone: true,
+	// 扬声器是否打开
+	speaker: false
   }
 
 export default {
   namespaced: true,
-  state: {
-	// 连接类型
-    rtc_type: "",
-	// 连接状态
-    rtc_status: "",
-	// 是否群聊
-	grounp: false,
-	// 连接的用户信息
-    rtc_info: undefined
-  },
+  state: default_state,
   mutations: {
 	  setState(state, data) {
 		  state = Object.assign(state, data)
